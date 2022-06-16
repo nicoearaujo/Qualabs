96d09af7b03c7661ac5badfb66f5ab5f1364540a
nicoearaujo <nicolasaraujo2611@gmail.com>
2022-06-16 15:59:08 -0300

    Solución ej 2

--- Helpers/helper.js
+++ Helpers/helper.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+const { groupCollapsed } = require('console');
+
 
 
 const cargarOutput = (files, res) => {
@@ -20,11 +22,12 @@ const cargarOutput = (files, res) => {
         res.content_module[json.provider.content_module].push(`./${file}`)
         res.auth_module[json.provider.auth_module].push(`./${file}`)
     })
+
 }
 
 
 
-exports.processData = function (folderName) {
+exports.processData = (folderName) => {
     let res = {
         auth_module: {},
         content_module: {}
@@ -34,5 +37,73 @@ exports.processData = function (folderName) {
     //Cargo los archivos del directorio
     const files = fs.readdirSync(folderPath)
     cargarOutput(files, res)
+    return res
+}
+
+
+
+//  PARTE B
+
+exports.obtenerGrupos = (folderName) => {
+    let res = new Set()
+    const folderPath = path.join(__dirname, '..', folderName);
+    //Cargo los archivos del directorio
+    const files = fs.readdirSync(folderPath)
+    files.forEach(file => {
+        let currentFile = fs.readFileSync(path.join(__dirname, '..', 'Input', file), 'utf8');
+        let json = JSON.parse(currentFile)
+        res.add(json.provider.content_module)
+        res.add(json.provider.auth_module)
+    })
+    return res
+}
+
+exports.mejorCandidato = (folderName, grupos) => {
+    const folderPath = path.join(__dirname, '..', folderName)
+    let mejorCandidato = undefined
+    //Cargo los archivos del directorio
+    const files = fs.readdirSync(folderPath)
+    const cantArchivos = files.length
+
+    //Recorro todos los usuarios
+    for (i = 0; i < cantArchivos; i++) {
+        let cantCoincidencias = 0
+        let currentFile = fs.readFileSync(path.join(__dirname, '..', folderName, files[i]), 'utf8');
+        let json = JSON.parse(currentFile)
+        //Si el provider no se utilizo aún le agrego coincidencia
+        if (grupos.has(json.provider.auth_module)) {
+            cantCoincidencias++
+        }
+        if (grupos.has(json.provider.content_module)) {
+            cantCoincidencias++
+        }
+
+        //Si es el mejor candidato lo guardo
+        if (!mejorCandidato || cantCoincidencias > mejorCandidato.cantCoincidencias) {
+            mejorCandidato = {
+                user: files[i],
+                module1: json.provider.auth_module,
+                module2: json.provider.content_module,
+                cantCoincidencias: cantCoincidencias
+            }
+        }
+    }
+
+    //Elimino el modulo del grupo
+    grupos.delete(mejorCandidato.module1)
+    grupos.delete(mejorCandidato.module2)
+
+    return mejorCandidato
+}
+
+
+exports.ej2 = (folderName) => {
+    let grupos = this.obtenerGrupos(folderName)
+    let res = new Set()
+
+    while (grupos.size > 0) {
+        let mejorCandidato = this.mejorCandidato(folderName, grupos)
+        res.add(mejorCandidato.user)
+    }
     return res
 }
--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { processData, ej2 } = require('./helpers/helper');
 
 
-const { processData } = require('./helpers/helper');
-
+//Nombres de los directorios de entrada y salida
 const INPUTFOLDERNAME = 'Input';
 const OUTPUTFOLDERNAME = "Output";
-
 const outputFolderPath = `./${OUTPUTFOLDERNAME}`;
 
 
 (() => {
     const ej1 = processData(INPUTFOLDERNAME)
     const pathEj1 = outputFolderPath + "/ej1.json"
+    const pathEj2 = outputFolderPath + "/ej2.json"
     fs.writeFileSync(pathEj1, JSON.stringify(ej1))
+    const eje2 = Array.from(ej2(INPUTFOLDERNAME))
+    fs.writeFileSync(pathEj2, JSON.stringify(eje2))
 })()
 
